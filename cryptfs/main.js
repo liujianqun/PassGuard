44e28f5024540c0ec3fc036cbec41046ad420526
Francis Maina <frankmaina@live.com>
2015-09-14 10:25:24 +0300

    Update main.js on cryptfs

--- cryptfs/main.js
+++ cryptfs/main.js
@@ -8,15 +8,17 @@ saveToFile: function(file_path,data,operation,extname) {
 	if (operation=='encrypt'){
 
 		var fileName = path.basename(file_path, path.extname(file_path));
-		var encrypted_fileName = 'examples/ENCRYPTED_'+fileName+'.'+extname;
+		var encrypted_fileName = 'examples/ENCRYPTED_'+fileName+'.'+extname; //not such a good way of doing this
+		//got to have some form of error handling here 
 		fs.writeFileSync(encrypted_fileName, data);
-		console.log("Successful");
+		console.log("Successful saved.");
 	}else if(operation=='decrypt'){
 
 		var fileName = path.basename(file_path, path.extname(file_path));
-		var encrypted_fileName = 'examples/DECRYPTED_file'.+extname;
+		var encrypted_fileName = 'examples/DECRYPTED_file'.+extname; //not such a good way of doing this
+		//got to have some form of error handling here 
 		fs.writeFileSync(encrypted_fileName, data);
-		console.log("Successful");
+		console.log("Successful saved.");
 	}
 
 }
