f21b0ac8d200bec1846cb33bc606873258d9c402
Francis Maina <frankmaina@live.com>
2015-09-14 10:22:17 +0300

    Update main.js

--- crypt/main.js
+++ crypt/main.js
@@ -2,14 +2,14 @@ var crypto= require('crypto');
     algorithm = 'aes-256-ctr';
 var ciphers = crypto.getCiphers();
 
-
+//somehow i know i dont have to repeat this operation twice
 module.exports = {
 
 encrypt: function (text,password){
   var cipher = crypto.createCipher(algorithm,password)
-  var crypted = cipher.update(text,'utf8','hex')
+  var crypted = cipher.update(text,'utf8','hex') //encrypt file to hex(hexadecimal)
   crypted += cipher.final('hex');
-  console.log("File has been encrypted. Writing to disc.");
+  console.log("File has been encrypted. ");
   return crypted;
 },
  
@@ -17,7 +17,7 @@ decrypt: function (text,password){
   var decipher = crypto.createDecipher(algorithm,password)
   var dec = decipher.update(text,'hex','utf8')
   dec += decipher.final('utf8');
-  console.log("the file was decrypted. Writing to disc.");
+  console.log("the file was decrypted.");
   return dec;
 }
  
